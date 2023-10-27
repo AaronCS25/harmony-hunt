0dd0df372c796dc039e74233ff56dae2fc262653
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-10-26 23:57:52 -0500

    datasource: modified | datasources & repositories

--- lib/domain/repositories/query_repository.dart
+++ lib/domain/repositories/query_repository.dart
@@ -1,18 +1,4 @@
-import 'package:ir2/domain/entities/entities.dart';
-
 abstract class QueryRepository {
   // * Obtener los ids de las canciones relevantes a una búsqueda.
   Future<List<String>> getTrackIds({String query});
-
-  // * Obtener la información de una canción.
-  Future<Song> getSong({String trackId});
-
-  // * Obtener la información de un conjunto de canciones.
-  Future<List<Song>> getSongsFromIds({List<String> trackIds});
-
-  // * Obtener la información de un album.
-  Future<Album> getAlbum({String trackAlbumId});
-
-  // * Obtener la información de un conjunto de albums.
-  Future<List<Album>> getAlbumsFromIds({List<String> trackAlbumIds});
 }
