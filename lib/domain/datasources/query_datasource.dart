084b7e6b56f65ebc6e8d0fd506394585e9e933e7
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-10-27 14:31:01 -0500

    domain/.../datasource & repositories: modified

--- lib/domain/datasources/query_datasource.dart
+++ lib/domain/datasources/query_datasource.dart
@@ -1,4 +1,6 @@
+import 'package:ir2/domain/entities/entities.dart';
+
 abstract class QueryDatasource {
   // * Obtener los ids de las canciones relevantes a una búsqueda.
-  Future<List<String>> getTrackIds(String query);
+  Future<List<OwnTrack>> getTrackIds(String query);
 }
--- lib/domain/repositories/query_repository.dart
+++ lib/domain/repositories/query_repository.dart
@@ -1,4 +1,6 @@
+import 'package:ir2/domain/entities/entities.dart';
+
 abstract class QueryRepository {
   // * Obtener los ids de las canciones relevantes a una búsqueda.
-  Future<List<String>> getTrackIds(String query);
+  Future<List<OwnTrack>> getTrackIds(String query);
 }
