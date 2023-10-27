d91158aaf077022a13eac58dd149754971e3dd5b
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-10-27 15:16:34 -0500

    lib\infraestructure\datasources: query_own & song_info added

---
+++ lib/infraestructure/datasources/query_own_datasource.dart
@@ -0,0 +1,38 @@
+import 'package:ir2/domain/datasources/query_datasource.dart';
+import 'package:ir2/infraestructure/models/models.dart';
+import 'package:ir2/domain/entities/entities.dart';
+import 'package:dio/dio.dart';
+
+class QueryOwnDatasource extends QueryDatasource {
+  final dio = Dio(
+    BaseOptions(
+      // TODO: Cambiar por la url de la api.
+      baseUrl: 'my-own-api.com',
+    ),
+  );
+
+  @override
+  Future<List<OwnTrack>> getTrackIds(String query) async {
+    try {
+      final response = await dio.get('/search', queryParameters: {'q': query});
+
+      if (response.statusCode == 200) {
+        final List<QueryTrackResponse> queryTracks = response.data['results']
+            .map<QueryTrackResponse>((result) => QueryTrackResponse.fromJson(result))
+            .toList();
+        final List<OwnTrack> ownTracks = queryTracks
+            .map<OwnTrack>((result) => OwnTrack(
+                  track_id: result.trackId,
+                  album_track_id: result.trackAlbumId,
+                  lyrics: result.lyrics,
+                ))
+            .toList();
+        return ownTracks;
+      } else {
+        throw Exception('Error al obtener los IDs de las canciones');
+      }
+    } catch (e) {
+      throw Exception('Error de red: $e');
+    }
+  }
+}
---
+++ lib/infraestructure/datasources/song_info_spotify_datasource.dart
@@ -0,0 +1,80 @@
+import 'package:ir2/domain/datasources/song_info_datasource.dart';
+import 'package:ir2/infraestructure/mappers/album_mapper.dart';
+import 'package:ir2/infraestructure/mappers/song_mapper.dart';
+import 'package:ir2/infraestructure/models/models.dart';
+import 'package:ir2/domain/entities/entities.dart';
+import 'package:dio/dio.dart';
+
+class SongInfoSpotifyDatasource extends SongInfoDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.spotify.com/v1/',
+    ),
+  );
+
+  @override
+  Future<Song> getSong(String trackId) async {
+    try {
+      final response = await dio.get('tracks/$trackId');
+
+      if (response.statusCode == 200) {
+        final trackByIdResponse = TrackByIdResponse.fromJson(response.data);
+        final song = SongMapper.spotifyTrackToEntity(trackByIdResponse);
+        return song;
+      } else {
+        throw Exception('Failed to load track');
+      }
+    } catch (e) {
+      throw Exception('Error getting track: $e');
+    }
+  }
+
+  @override
+  Future<List<Song>> getSongsFromIds(List<String> trackIds) async {
+    try {
+      final List<Future<Song>> songFutures = [];
+
+      for (String trackId in trackIds) {
+        songFutures.add(getSong(trackId));
+      }
+
+      final List<Song> songs = await Future.wait(songFutures);
+      return songs;
+    } catch (e) {
+      throw Exception('Error getting tracks: $e');
+    }
+  }
+
+  @override
+  Future<Album> getAlbum(String trackAlbumId) async {
+    try {
+      final response = await dio.get('albums/$trackAlbumId');
+
+      if (response.statusCode == 200) {
+        final albumByIdResponse = AlbumByIdResponse.fromJson(response.data);
+        final album = AlbumMapper.spotifyAlbumToEntity(albumByIdResponse);
+        return album;
+      } else {
+        throw Exception('Failed to load album');
+      }
+    } catch (e) {
+      throw Exception('Error getting album: $e'); 
+    }
+  }
+
+  @override
+  Future<List<Album>> getAlbumsFromIds(List<String> trackAlbumIds) async {
+    try {
+      final List<Future<Album>> albumFutures = [];
+
+      for (String trackAlbumId in trackAlbumIds) {
+        albumFutures.add(getAlbum(trackAlbumId));
+      }
+
+      final List<Album> albums = await Future.wait(albumFutures);
+      return albums;
+    } catch (e) {
+      throw Exception('Error getting albums: $e');
+    }
+  }
+}
