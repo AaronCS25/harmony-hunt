36bcea5a22b250e4be6b7f2201fa9d4a227229b0
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-10-27 11:43:11 -0500

    mappers/song_mapper.dart

---
+++ lib/infraestructure/mappers/song_mapper.dart
@@ -0,0 +1,20 @@
+import 'package:ir2/domain/entities/entities.dart';
+import 'package:ir2/infraestructure/models/models.dart';
+
+class SongMapper {
+  static Song spotifyTrackToEntity(TrackByIdResponse track) => Song(
+        title: track.name,
+        artistName: track.artists.first.name,
+        urlArtistPageSpotify: track.externalUrls.spotify,
+        albumName: track.album.name,
+        albumType: track.album.albumType,
+        urlAlbumSpotify: track.album.externalUrls.spotify,
+        releaseDate: track.album.releaseDate.toString(), // TODO: FIX IT
+        songDuration: track.durationMs,
+        trackNumberOnTheAlbum: track.trackNumber,
+        popularity: track.popularity,
+        urlToSongSpotify: track.externalUrls.spotify,
+        urlCover:
+            track.album.images.isNotEmpty ? track.album.images.first.url : '',
+      );
+}
