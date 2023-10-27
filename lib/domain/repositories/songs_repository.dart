70eeb99209e0bccbef6bf7fe408c04781c0c900d
AaronCS25 <aaron.camacho@utec.edu.pe>
2023-10-26 23:37:35 -0500

    datasource: modified | datasources & repositories

--- lib/domain/repositories/songs_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:ir2/domain/entities/entities.dart';
-
-abstract class SongRepository {
-  Future<List<String>> getTrackIds({String query});
-  Future<List<Song>> getSong({String trackId});
-  Future<List<Song>> getAlbumSongs({String trackAlbumId});
-}
